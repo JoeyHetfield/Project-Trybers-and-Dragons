842f6bb4b9fb95f2b84387d5a0922a93a54cfe6f
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-05 15:37:01 -0300

    Requsiito 1 feito, criado index.ts também

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,33 +1,26 @@
-// O arquivo foi criado no diretório src/Races/ e chamar Race.ts;
-// name e dexterity devem ser recebidos como parâmetros e inicializados no construtor.
-// Os atributos da classe Race podem ser lidos, mas não podem ser alterados:
-// name deve retornar o tipo string;
-// dexterity deve retornar o tipo number.
-// A classe Race deve ter um método estático chamado createdRacesInstances, que retorna um number;
-// Esse número corresponde à quantidade de instâncias criadas a partir das classes estendidas da classe Race;
-// Cada raça terá seu número máximo de instâncias, que será definido dentro de cada classe especializada;
-// Na classe Race, o método deve lançar um erro com a mensagem Not implemented.
-// A classe Race deve ter um getter abstrato chamado maxLifePoints que retorna um number;
-// Esse número corresponde à quantidade máxima de pontos de vida da raça;
-// Cada raça terá seu número máximo de pontos, que será definido dentro de cada classe especializada;
-// Na classe Race deve estar apenas a assinatura do método.
-
-class Race {
-  private name: string;
-  private dexterity: number;
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
 
   constructor(name: string, dexterity: number) {
-    this.name = name;
-    this.dexterity = dexterity;
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+  // não entendi porque o metodo tem que ser name e não getName, o mesmo pro dexterity
+
+  get name(): string {
+    return this._name;
   }
 
-  public get getName(): string {
-    return this.name;
+  get dexterity(): number {
+    return this._dexterity;
   }
 
-  public get getDexterity(): number {
-    return this.dexterity;
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
   }
+
+  abstract get maxLifePoints(): number;
 }
 
 export default Race;
