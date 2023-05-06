c782d5ad67fe2a7e80007a568a07d33a3de44c0f
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-06 11:25:57 -0300

    iniciando characters, falta constructor e metodos

---
+++ src/Character.ts
@@ -0,0 +1,19 @@
+import Fighter from './Fighter';
+import Race from './Races';
+import Archetype from './Archetypes';
+import Energy from './Energy';
+import getRandomInt from './utils';
+
+class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+  private _name: string;
+
+
+}
