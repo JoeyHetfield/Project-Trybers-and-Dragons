f7c572c72f8c3b4f1d4e03dd261c0b542aa7e914
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-06 11:40:26 -0300

    constructor character, falta os metodos

--- src/Character.ts
+++ src/Character.ts
@@ -3,17 +3,34 @@ import Race from './Races';
 import Archetype from './Archetypes';
 import Energy from './Energy';
 import getRandomInt from './utils';
+import Elf from './Races/Elf';
+import Mage from './Archetypes/Mage';
 
 class Character implements Fighter {
   private _race: Race;
   private _archetype: Archetype;
   private _maxLifePoints: number;
-  private _lifePoints: number;
-  private _strength: number;
-  private _defense: number;
+  private lifePoints: number;
+  private strength: number;
+  private defense: number;
   private _dexterity: number;
   private _energy: Energy;
   private _name: string;
 
-
+  constructor(name: string) {
+    this._name = name;
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(this._name, this._dexterity);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this.lifePoints = this._maxLifePoints;
+    this.strength = getRandomInt(1, 10);
+    this.defense = getRandomInt(1, 10);
+    this._archetype = new Mage(this._name);
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
 }
+
+export default Character;
