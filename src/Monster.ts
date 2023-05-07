58732915fbece118abd28211ddd68e08f7bed029
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-07 14:51:17 -0300

    classe monster criada com sucesso, mas falta a interaçãocom character

---
+++ src/Monster.ts
@@ -0,0 +1,39 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 83;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    } else {
+      this._lifePoints -= 1;
+    }
+    if (this.lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this.lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): number {
+    const damage = this.strength;
+    return enemy.receiveDamage(damage);
+  }
+}
+
+export default Monster;
