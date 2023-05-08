cc56b4dfda9695829552a5d8086c1689f1cf794a
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-08 16:34:17 -0300

    class PVP feito

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,10 +1,29 @@
-// import Battle from './Battle';
-// import Fighter from '../Fighter';
+import Battle from './Battle';
+import Fighter from '../Fighter';
 
-// class PVP extends Battle {
-//   constructor(player: Fighter, opponent: Fighter) {
-//     super(player, opponent);
-//   }
-// }
+class PVP extends Battle {
+  private _player: Fighter;
+  private _opponent: Fighter;
+  constructor(player: Fighter, opponent: Fighter) {
+    super(player);
+    this._player = player;
+    this._opponent = opponent;
+  }
 
-// export default PVP;
+  playersHaveLife() : boolean {
+    return this._player.lifePoints > 0 && this._opponent.lifePoints > 0;
+  }
+
+  fight(): number {
+    while (this.playersHaveLife()) {
+      if (this.player.lifePoints > 0) {
+        this._player.attack(this._opponent);
+      }
+      if (this._opponent.lifePoints > 0) {
+        this._opponent.attack(this._player);
+      }
+    } return super.fight();
+  }
+}
+
+export default PVP;
